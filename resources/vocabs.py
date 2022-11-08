e5963a79b17767898c4431d2fc4d3f2b5aa6d36d
apriljsu <apriljsu@gmail.com>
2022-11-08 00:00:17 -0500

    show and  update routes created for vocab

--- resources/vocabs.py
+++ resources/vocabs.py
@@ -16,11 +16,37 @@ def get_all_vocabs():
 
 @vocab.route('/', methods=['POST'])
 def create_vocab():
-    payload = request.get_json()
-    print(type(payload),'payload')
+    payload = request.get_json() 
+    # print(type(payload),'payload type') #payload type is class 'dict'
+    # print(payload, 'payload')#only shows the vocab card added without ID
     vocab = models.Vocab.create(**payload)
-    print(vocab.__dict__)
-    print(dir(vocab))
+    # print(type(vocab)) # <model:Vocab>
+    # print(vocab.__dict__)
+    # print(dir(vocab)) #The dir() function returns all properties and methods of the specified object, without the values.
     print(model_to_dict(vocab),'model to dict')
     vocab_dict=model_to_dict(vocab)
     return jsonify(data=vocab_dict, status={'code':201,'message':'Success'} )
+
+@vocab.route('/<id>', methods=['GET'])
+def get_one_vocab(id):
+    # print(id, 'vocab id')
+    vocab = models.Vocab.get_by_id(id)
+    # print(vocab) # only shows ID
+    print(vocab.__dict__)
+    return jsonify(
+        data = model_to_dict(vocab),
+        status=200,
+        message ='Success'
+    ), 200
+
+@vocab.route('/<id>', methods=['PUT'])
+def update_vocab(id):
+    payload = request.get_json()
+    query = models.Vocab.update(**payload).where(models.Vocab.id==id)
+    query.execute()
+    return jsonify(
+        data = model_to_dict(models.Vocab.get_by_id(id)),
+        status = 200,
+        message = 'vocab updated successfully'
+    ), 200
+
