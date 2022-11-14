42892fee4d45935ad136a9c66024b06eb27fa899
apriljsu <apriljsu@gmail.com>
2022-11-13 23:16:15 -0500

    user model added

--- models.py
+++ models.py
@@ -1,7 +1,22 @@
 # No.2 start
 from peewee import *
 import datetime
+
+# No. 4 starts
+from flask_login import UserMixin
+
+
+#No.2 continue
 DATABASE = SqliteDatabase('vocabs.sqlite')
+class User(UserMixin, Model):
+    username = CharField(unique = True)
+    email = CharField(unique = True)
+    password = CharField()
+    first_name = CharField()
+    last_name = CharField()
+    class Meta:
+        database = DATABASE
+
 class Vocab(Model):
     vocab_chinese = CharField(unique=True)
     vocab_english = CharField(unique=True)
@@ -9,8 +24,9 @@ class Vocab(Model):
     set = IntegerField()
     class Meta:
         database = DATABASE
+
 def initialize():
     DATABASE.connect()
-    DATABASE.create_tables([Vocab], safe=True)
+    DATABASE.create_tables([Vocab, User], safe=True)
     print('tables created')
     DATABASE.close()
