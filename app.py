dc0d8a541e001fdb4366f2b5115419e71d9a9265
apriljsu <apriljsu@gmail.com>
2022-11-21 01:57:07 -0500

    ready for deployment

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # No.1 start
-from flask import Flask, jsonify
+from flask import Flask, jsonify, after_this_request
 
 # No.3 continue here
 from flask_cors import CORS
@@ -45,6 +45,33 @@ app.register_blueprint(vocab, url_prefix = '/api/v1/vocab')
 #no.6 continue here
 CORS(user, origins = ['http://localhost:3000'], supports_credentials=True)
 app.register_blueprint(user, url_prefix = '/api/v1/user')
+
+# we don't want to hog up the SQL connection pool
+# so we should connect to the DB before every request
+# and close the db connection after every request
+
+@app.before_request # use this decorator to cause a function to run before reqs
+def before_request():
+
+    """Connect to the db before each request"""
+    print("you should see this before each request") # optional -- to illustrate that this code runs before each request -- similar to custom middleware in express.  you could also set it up for specific blueprints only.
+    models.DATABASE.connect()
+
+    @after_this_request # use this decorator to Executes a function after this request
+    def after_request(response):
+        """Close the db connetion after each request"""
+        print("you should see this after each request") # optional -- to illustrate that this code runs after each request
+        models.DATABASE.close()
+        return response # go ahead and send response back to client
+                      # (in our case this will be some JSON)
+
+# ADD THESE THREE LINES -- because we need to initialize the
+# tables in production too!
+if os.environ.get('FLASK_ENV') != 'development':
+  print('\non heroku!')
+  models.initialize()
+
+
 #run the app when the program starts
 #this is like app.listen() in express - it goes at the bottom 
 #__name__ == '__main__' here means we just ran this file from the command line,as opposed to exporting and importing from somewhere else
--- models.py
+++ models.py
@@ -4,10 +4,21 @@ import datetime
 
 # No. 4 starts : import usermixin for user model
 from flask_login import UserMixin
+import os
+from playhouse.db_url import connect
+
 
 
 #No.2 continue: set up vocabs model
-DATABASE = SqliteDatabase('vocabs.sqlite')
+# DATABASE = SqliteDatabase('vocabs.sqlite')
+if 'ON_HEROKU' in os.environ: # later we will manually add this env var
+                              # in heroku so we can write this code
+  DATABASE = connect(os.environ.get('DATABASE_URL')) # heroku will add this
+                                                     # env var for you
+                                                     # when you provision the
+                                                     # Heroku Postgres Add-on
+else:
+  DATABASE = SqliteDatabase('vocabs.sqlite')
 
 #No. 5 start: set up user model
 class User(UserMixin, Model):   
